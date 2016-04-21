ec378e00e3454394a4e195061e1fff430ec3bc16
AdamDeHovitz <adam.dehovitz@gmail.com>
2016-04-21 16:37:18 -0400

    messeging

--- src/main/java/edu/brown/cs/user/Entities/Chat/Message.java
+++ src/main/java/edu/brown/cs/user/Entities/Chat/Message.java
@@ -6,6 +6,7 @@ import edu.brown.cs.user.Entities.Account.Account;
  * Created by adamdeho on 4/10/16.
  */
 public class Message {
+  private String id;
   private Account user;
   private String content;
   private String time;
