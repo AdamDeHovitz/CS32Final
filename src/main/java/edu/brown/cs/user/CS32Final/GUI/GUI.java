d6fa618e7ad3155ba38a5a3299d3052227c5f942
AdamDeHovitz <adam.dehovitz@gmail.com>
2016-04-21 17:18:29 -0400

    merging

--- src/main/java/edu/brown/cs/user/CS32Final/GUI/GUI.java
+++ src/main/java/edu/brown/cs/user/CS32Final/GUI/GUI.java
@@ -1,7 +1,115 @@
 package edu.brown.cs.user.CS32Final.GUI;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import edu.brown.cs.user.CS32Final.Entities.Account.Account;
+import edu.brown.cs.user.CS32Final.SQL.SqliteDatabase;
+import freemarker.template.Configuration;
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by adamdeho on 4/21/16.
  */
 public class GUI {
+
+  private final Gson gson = new Gson();
+
+
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf(
+              "ERROR: Unable use %s for template loading.\n", templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  /**
+   * Runs the spark server.
+   */
+  private void runSparkServer() {
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+
+    FreeMarkerEngine freeMarker = createEngine();
+
+    // Setup Spark Routes
+    // Setup Spark Routes
+    Spark.get("/", new FrontHandler(), freeMarker);
+    Spark.post("/login", new LoginHandler());
+  }
+
+  /**
+   * Exception Printer for errors I think. I didn't write it.
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
+  }
+
+  /**
+   * Handles the main page.
+   */
+  private class FrontHandler implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      Map<String, Object> variables =
+              ImmutableMap.of("title", "Type Away!", "content", "");
+      return new ModelAndView(variables, "main.ftl");
+    }
+  }
+
+  /**
+   * For logging in
+   */
+  private class LoginHandler implements Route {
+    @Override
+    public Object handle(final Request req, final Response res) {
+      QueryParamsMap qm = req.queryMap();
+      String username = qm.value("username");
+      String password = qm.value("password");
+
+      Account user =  SqliteDatabase.findUserByUsername(username);
+      Map<String, Object> variables = null;
+      if (user.authenticate(password)) {
+        variables = user.getData();
+
+      }
+      else {
+        System.out.println("we need something done here");
+      }
+
+
+      return gson.toJson(variables);
+    }
+  }
 }
