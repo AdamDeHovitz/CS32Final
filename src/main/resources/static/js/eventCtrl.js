03fae59bcbfca1798e6568e2a4e4d38cde989c45
Leslie Bresnahan <lbresna1@cs.brown.edu>
2016-04-25 16:34:29 -0400

    fixed integration prob with joining and leaving event

--- src/main/resources/static/js/eventCtrl.js
+++ src/main/resources/static/js/eventCtrl.js
@@ -32,7 +32,9 @@ bulkAppControllers.controller("eventCtrl",
 		console.log("joining");
 		$scope.isMember = true;
 		$scope.event.CurMemberNum = $scope.event.CurMemberNum + 1;
-		$.post("/event-join", {id: $rootScope.account.id, eventId: $scope.eventId}, 
+		var postParams = {id: $rootScope.account.id, eventId: eventId};
+		console.log(postParams);
+		$.post("/event-join", postParams, 
 			function(responseJSON) {
 				responseObject = JSON.parse(responseJSON);
 				console.log(responseObject);
@@ -46,6 +48,11 @@ bulkAppControllers.controller("eventCtrl",
 	$scope.confirmLeave = function() {
 		console.log("leaving");
 		$scope.event.CurMemberNum = $scope.event.CurMemberNum - 1;
+		$.post("/event-remove", postParams, 
+				function(responseJSON) {
+					responseObject = JSON.parse(responseJSON);
+					console.log(responseObject);
+		});
 
 		$scope.isMember = false;
 	}
