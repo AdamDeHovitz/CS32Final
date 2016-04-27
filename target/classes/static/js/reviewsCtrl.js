db71b8c64f36c38389d09d7d946990619cf04138
Leslie Bresnahan <lbresna1@cs.brown.edu>
2016-04-27 19:48:48 -0400

    set up view for messages with lots of fancy and responsive css

--- target/classes/static/js/reviewsCtrl.js
+++ target/classes/static/js/reviewsCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 bulkAppControllers.controller("reviewsCtrl", function($scope, $rootScope, $http, $stateParams) {
 	$scope.curTitle = "Reviews";
 	var profileId = $stateParams.userId;
@@ -12,18 +11,6 @@ bulkAppControllers.controller("reviewsCtrl", function($scope, $rootScope, $http,
 	$scope.reviews = [{rating: 4, text: "HE COOL YO"},
 	                  {rating: 1, text: "dis bitch fucked me over"}];
 
-=======
-bulkAppControllers.controller("reviewsCtrl", function($scope, $rootScope, $http) {
-	$scope.curTitle = "Reviews";
-	var profileId = $stateParams.userId;
-
-	$.post("/reviews", {id: profileId}, function(responseJSON) {
-			responseObject = JSON.parse(responseJSON);
-			$scope.curReviews = responseObject.reviews;
-			console.log(responseObject);
-	});
-
->>>>>>> 6ee9b243c73fac568a75d5c46fde5298ae4b4609
 	
 
 		/*
