0abe5b956086a706b2e2c85d1a2c9e56a2a8cfb2
Adam DeHovitz <adam@privicee.com>
2016-08-02 17:35:24 -0400

    change port

--- src/main/java/edu/brown/cs/user/CS32Final/GUI/GUI.java
+++ src/main/java/edu/brown/cs/user/CS32Final/GUI/GUI.java
@@ -80,7 +80,7 @@ public class GUI {
     if (processBuilder.environment().get("PORT") != null) {
       return Integer.parseInt(processBuilder.environment().get("PORT"));
     }
-    return 4567; // return default port if heroku-port isn't set (i.e. on
+    return 80; // return default port if heroku-port isn't set (i.e. on
                  // localhost)
   }
 
